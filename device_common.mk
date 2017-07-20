4bb6101d5a37cf5679e28726aa42a5a0925059f7
Dhruv Gadhavi <dg28gadhavi@gmail.com>
2017-07-20 17:33:52 +0530

    Added Filesystem management tools

--- device_common.mk
+++ device_common.mk
@@ -209,15 +209,20 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.sys.sdcardfs=true
 
 # Filesystem management tools
-	PRODUCT_PACKAGES += \
-	    e2fsck \
-	    fibmap.f2fs \
-	    fsck.f2fs \
-	    mkfs.f2fs \
-	    make_ext4fs \
-	    resize2fs \
-	    setup_fs \
-	    mount.exfat \
+PRODUCT_PACKAGES += \
+    e2fsck \
+    fibmap.f2fs \
+    fsck.f2fs \
+    mkfs.f2fs \
+    make_ext4fs \
+    resize2fs \
+    setup_fs \
+    mount.exfat \
+    fsck.exfat \
+    mkfs.exfat \
+    fsck.ntfs \
+    mkfs.ntfs \
+    mount.ntfs
 
 # Properties
 	PRODUCT_PROPERTY_OVERRIDES += \
