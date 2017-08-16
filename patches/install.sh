39bf1cf0175661cb546b720b158ca27bbf08c237
Dhruv Gadhavi <dg28gadhavi@gmail.com>
2017-08-16 19:36:29 +0000

    Update Patches

--- patches/install.sh
+++ patches/install.sh
@@ -2,7 +2,7 @@ echo $1
 rootdirectory="$PWD"
 # ---------------------------------
 
-dirs="bionic frameworks/base frameworks/av frameworks/native packages/apps/Settings packages/apps/Dialer packages/services/Telephony system/netd system/core hardware/libhardware "
+dirs="bionic frameworks/base frameworks/av frameworks/native packages/apps/Settings packages/apps/Dialer packages/services/Telephony system/netd system/sepolicy system/core hardware/libhardware "
 
 for dir in $dirs ; do
 	cd $rootdirectory
