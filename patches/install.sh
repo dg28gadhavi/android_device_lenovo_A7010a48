abf23046487a940c6fc632cc5ab6db8a41f10614
Dhruv Gadhavi <dg28gadhavi@gmail.com>
2017-08-20 11:46:36 +0000

    Update the patches to be applied

--- patches/install.sh
+++ patches/install.sh
@@ -2,7 +2,7 @@ echo $1
 rootdirectory="$PWD"
 # ---------------------------------
 
-dirs="system/core"
+dirs="bionic frameworks/base frameworks/av frameworks/native packages/apps/Settings packages/apps/Dialer packages/services/Telephony system/netd system/sepolicy system/core hardware/libhardware"
 
 for dir in $dirs ; do
 	cd $rootdirectory
