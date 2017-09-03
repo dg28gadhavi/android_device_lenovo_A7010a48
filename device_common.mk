3245a1637abafd65978e52c12f4234c074f6cf34
Dhruv Gadhavi <dg28gadhavi@gmail.com>
2017-09-03 22:22:21 +0000

    Once Upon A Time..

--- device_common.mk
+++ device_common.mk
@@ -216,7 +216,7 @@ PRODUCT_PACKAGES += \
 
 #Fingerprint HAL
 PRODUCT_PACKAGES += \
-    fingerprintd
+   android.hardware.biometrics.fingerprint@2.1-service
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:system/etc/permissions/android.hardware.fingerprint.xml
