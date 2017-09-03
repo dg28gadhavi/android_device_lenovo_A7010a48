22bf4c4e38a398361cc5fbc8352de6c25e36e899
Dhruv Gadhavi <dg28gadhavi@gmail.com>
2017-09-03 22:39:21 +0000

    Compile Power HAL

--- device_common.mk
+++ device_common.mk
@@ -146,6 +146,10 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
     android.hardware.audio.effect@2.0-impl
 
+# Power
+PRODUCT_PACKAGES += \
+    android.hardware.power@1.0-impl
+
 # Camera
 PRODUCT_PACKAGES += \
     libcamera_parameters_ext
