d73f7108ad279efd24f92cacce2cd37101fcd315
Dhruv Gadhavi <dg28gadhavi@gmail.com>
2017-09-10 13:07:47 +0000

    Will See later on...
    This reverts commit ef11ed3b36fdc5ef3e7b5137c80290edd0429f89.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -237,7 +237,7 @@ BOARD_SECCOMP_POLICY += device/lenovo/A7010a48/seccomp
 MTK_GPU_VERSION := mali midgard r12p1
 #MTK_GPU_VERSION := mali midgard r7p0
 MTK_HWC_SUPPORT := yes
-MTK_HWC_VERSION := 1.5.0
+MTK_HWC_VERSION := 1.4.1
 
 # Renderer
 USE_OPENGL_RENDERER := true
